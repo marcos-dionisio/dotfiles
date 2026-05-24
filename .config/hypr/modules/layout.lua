846ab28a6111d6bbc3c3a4c9731360bda5eb126b
Marcos Dionisio <marcos.dionisio@proton.me>
2026-05-24 10:12:28 -0400

    refactor(hyprland): separate layout configuration into the modules folder

---
+++ .config/hypr/modules/layout.lua
@@ -0,0 +1,29 @@
+-- Layout config
+
+hl.config({
+    general = {
+        gaps_in = 5,
+        gaps_out = 10,
+        border_size = 2,
+        resize_on_border = true,
+
+        layout = "dwindle",
+
+        col = {
+            active_border = "rgba(137, 220, 235, 1)",
+            inactive_border = "rgba(59, 59, 59, 1)",
+        },
+    },
+
+    decoration = {
+        rounding = 5,
+        rounding_power = 2,
+
+        blur = {
+            enabled   = true,
+            size      = 2,
+            passes    = 1,
+            vibrancy  = 0.1696,
+        },
+    },
+})

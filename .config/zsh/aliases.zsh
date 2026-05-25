485ca13233968ae24ebb40c5bb29c563f556c830
Marcos Dionisio <marcos.dionisio@proton.me>
2026-05-25 17:06:54 -0400

    refactor(zsh): change ls aliases to eza

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -3,11 +3,6 @@
 alias cp="cp -i"
 
 alias ~="cd ~"
-
-alias ls="ls --color"
-alias la="ls --color -a"
-alias ll="ls --color -la"
-
 alias ..="cd ../"
 alias ...="cd ../../"
 
@@ -18,9 +13,16 @@ alias .4="cd ../../../../"
 alias .5="cd ../../../../../"
 alias .6="cd ../../../../../../"
 
-alias gitdot="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+alias ls="eza --group-directories-first --icons"
+alias la="eza --group-directories-first --icons -a"
+alias ll="eza --group-directories-first --icons -la"
 
+alias cat="bat --theme \"Catppuccin Mocha\" --style plain"
+
+alias tree="eza --group-directories-first --icons --tree -a"
+
+alias gitdot="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
 alias mirrors="sudo reflector -c Brazil -p https --sort rate --save /etc/pacman.d/mirrorlist --verbose && sudo pacman -Syy"
 
-alias fetch="fastfetch --config arch"
 alias reset="clear && source $HOME/.zshrc"
+alias fetch="fastfetch --config arch"

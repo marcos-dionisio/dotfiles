027e9ac169a0de000895d762590cf336cb017fce
Marcos Dionisio <marcos.dionisio@proton.me>
2026-05-24 20:31:41 -0400

    feat(zsh): add new aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,7 +1,21 @@
 # Aliases
 
+alias cp="cp -i"
+
 alias ls="ls --color"
 alias la="ls --color -a"
 alias ll="ls --color -la"
 
+alias ..="cd ../"
+alias ...="cd ../../"
+
+alias .1="cd ../"
+alias .2="cd ../../"
+alias .3="cd ../../../"
+alias .4="cd ../../../../"
+alias .5="cd ../../../../../"
+alias .6="cd ../../../../../../"
+
 alias gitdot="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+
+alias mirrors="sudo reflector -c Brazil -p https --sort rate --save /etc/pacman.d/mirrorlist --verbose && sudo pacman -Syy"

12f5f827c8f55e9f14f09c8d15468db5db280ab9
Marcos Dionisio <marcos.dionisio@proton.me>
2026-05-31 10:11:24 -0400

    chore(zsh): remove mirrors alias

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -23,7 +23,6 @@ alias cat="bat --theme \"Catppuccin Mocha\" --style plain"
 alias tree="eza --group-directories-first --icons --tree -a"
 
 alias gitdot="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
-alias mirrors="sudo reflector -c Brazil -p https --sort rate --save /etc/pacman.d/mirrorlist --verbose && sudo pacman -Syy"
 
 alias reset="clear && source $HOME/.zshrc"
 alias fetch="fastfetch --config arch"

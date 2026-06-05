6bd4c72e7970774202e01e211aa0fd1ebc92d2dc
Marcos Dionisio <marcos.dionisio@proton.me>
2026-06-05 14:45:00 -0400

    refactor(dotfiles): add paru to install script

--- .local/share/dotfiles/install.bash
+++ .local/share/dotfiles/install.bash
@@ -1,32 +1,50 @@
 #!/usr/bin/bash
 
-sudo pacman -Sy \
-    brightnessctl \
-    bat \
-    dolphin \
-    eza \
-    fastfetch \
-    firefox \
-    fzf \
-    git \
-    hyprland \
-    hyprlauncher \
-    hyprpaper \
-    hyprshot \
-    kitty \
-    lua-language-server \
-    neovim \
-    noto-fonts \
-    noto-fonts-cjk \
-    noto-fonts-emoji \
-    noto-fonts-extra \
-    tree-sitter-cli \
-    ttf-firacode-nerd \
-    zsh \
-    zoxide \
+install_paru() {
+    sudo pacman -S --needed base-devel git
+    git clone https://aur.archlinux.org/paru.git /tmp/paru
+    cd /tmp/paru
+    makepkg -si
+}
 
-zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
-curl -s https://ohmyposh.dev/install.sh | bash -s
+install_dependencies() {
+    paru -Sy \
+        hyprland \
+        hyprlauncher \
+        hyprlock \
+        hyprpaper \
+        hyprshot \
+        noto-fonts \
+        noto-fonts-cjk \
+        noto-fonts-emoji \
+        noto-fonts-extra \
+        kitty \
+        dolphin \
+        firefox \
+        brightnessctl \
+        zsh \
+        zoxide \
+        eza \
+        bat \
+        fzf \
+        fastfetch \
+        neovim \
+        lua-language-server \
 
-git clone https://github.com/marcos-dionisio/dotfiles.git --bare $HONE/.dotfiles
-git --work-tree=$HOME --git-dir=$HOME/.dotfiles checkout -f
+    zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
+    curl -s https://ohmyposh.dev/install.sh | bash -s
+}
+
+install_dotfiles() {
+    git clone https://github.com/marcos-dionisio/dotfiles.git --bare $HONE/.dotfiles
+    git --work-tree=$HOME --git-dir=$HOME/.dotfiles checkout -f
+}
+
+change_shell() {
+    sudo usermod --shell /usr/bin/zsh $USERNAME
+}
+
+install_paru
+install_dependencies
+install_dotfiles
+change_shell
